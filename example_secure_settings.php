a059bcee61597ddc1c6fbcda1db2b75a812a7138
Curtis Ogle <curtis.jams.ogle@gmail.com>
2020-06-28 21:52:25 -0400

    Update settings in order to bootstrap the site.

---
+++ example_secure_settings.php
@@ -0,0 +1,10 @@
+<?php
+
+$_ENV['SITE_ENV'] = 'prod';
+$_ENV['SITE_DB_NAME'] = 'tome';
+$_ENV['SITE_DB_USER'] = 'vagrant';
+$_ENV['SITE_DB_PASS'] = 'vagrant';
+$_ENV['SITE_DB_HOST'] = 'local.tome.com';
+$_ENV['SITE_DB_PORT'] = '3306';
+$_ENV['SITE_TOME_DIRECTORY'] = '../html';
+$_ENV['SITE_GITHUB_ACCESS_TOKEN'] = 'Your GitHub Persional Access Token';
